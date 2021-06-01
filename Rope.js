c9afd4a6a7836a4e9ac8bb570e325b9860f56b5e
satyamsingh2008120 <75066139+satyamsingh2008120@users.noreply.github.com>
2021-06-01 15:55:32 +0530

    Add files via upload

---
+++ Rope.js
@@ -0,0 +1,29 @@
+class Rope{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 1.0,
+            length: 300
+        }
+        this.pointB = pointB;
+        this.rope = Constraint.create(options);
+        World.add(world, this.rope);
+    }
+    attach(body){
+       this.rope.bodyA = body
+    }
+    fly(){
+        this.rope.bodyA = null;           
+    }
+    display(){
+        if(this.rope.bodyA){
+        var pointA = this.rope.bodyA.position;
+        var pointB = this.pointB;
+        push ()
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        pop ()
+    }
+}
+}
---
+++ sketch.js
@@ -0,0 +1,77 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+var ground
+var engine, world;
+var box1, box2, box3, box4, box5, box6, box7
+var box8, box9, box10
+var box11, box12, box13, box14, box15, box16, box17
+
+var ball
+var rope
+function setup(){
+    var canvas = createCanvas(1500,800);
+    engine = Engine.create();
+    world = engine.world;
+
+
+    ground = new Ground(600,790,2000,20);
+    box1 = new Box(900,100,70,70)
+    box2 = new Box(900,100,70,70)
+    box3 = new Box(900,100,70,70)
+    box4 = new Box(900,100,70,70)
+    box5 = new Box(900,100,70,70)
+    box6 = new Box(900,100,70,70)
+    box7 = new Box(900,100,70,70)
+    
+    box8 = new Box(800,100,70,70)
+    box9 = new Box(800,100,70,70)
+    box10 = new Box(800,100,70,70)
+
+    box11 = new Box(700,100,70,70)
+    box12 = new Box(700,100,70,70)
+    box13 = new Box(700,100,70,70)
+    box14 = new Box(700,100,70,70)
+    box15 = new Box(700,100,70,70)
+    box16 = new Box(700,100,70,70)
+    box17 = new Box(700,100,70,70)
+    box18 = new Box(700,100,70,70)
+
+    ball = new Ball(200,200,40,40)
+    rope = new Rope(ball.body, {x:500,y:300})
+}
+
+function draw(){
+    background('yellow');
+    Engine.update(engine);
+  ground.display()   
+  box1.display()  
+  box2.display()  
+  box3.display()  
+  box4.display()  
+  box5.display()  
+  box6.display()  
+  box7.display() 
+
+  box8.display()
+  box9.display()
+  box10.display()
+  
+  box11.display()
+  box12.display()
+  box13.display()
+  box14.display()
+  box15.display()
+  box16.display()
+  box17.display()
+  box18.display()
+
+  ball.display()
+  rope.display()
+}
+
+function mouseDragged(){
+    Matter.Body.setPosition(ball.body,{x: mouseX, y: 200})
+    
+}
